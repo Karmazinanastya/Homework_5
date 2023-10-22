3da3cf25bc4cabfb747c1e90b1e4eb91a6c71a2a
Karmazinanastya <karmazina133@gmail.com>
2023-10-22 17:44:21 +0300

    Add project files.

---
+++ Homework_5/IDeveloper.cs
@@ -0,0 +1,45 @@
+﻿namespace Homework_5
+{
+    public interface IDeveloper
+    {
+        string Tool { get; set; }
+
+        void Create();
+        void Destroy();
+    }
+    class Programmer : IDeveloper
+    {
+        public string Language { get; set; }
+
+        public string Tool { get; set; }
+
+        public void Create()
+        {
+            Console.WriteLine("Програмiст пише код");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine("Програмiст видаляє код");
+        }
+    }
+
+    class Builder : IDeveloper
+    {
+        public string Equipment { get; set; }
+
+        public string Tool { get; set; }
+
+        public void Create()
+        {
+            Console.WriteLine("Будiвельник щось будує");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine("Будiвельник щось руйнує");
+        }
+    }
+
+}
+
---
+++ Homework_5/Program.cs
@@ -0,0 +1,42 @@
+﻿using Homework_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IDeveloper> developers = new List<IDeveloper>
+        {
+            new Programmer { Language = "C#", Tool = "Visual Studio" },
+            new Builder { Equipment = "Hammer", Tool = "Construction site tool" }
+        };
+
+        foreach (var developer in developers)
+        {
+            developer.Create();
+            developer.Destroy();
+        }
+
+        Dictionary<uint, string> idNameDictionary = new Dictionary<uint, string>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Enter ID: ");
+            uint id = uint.Parse(Console.ReadLine());
+
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+
+            idNameDictionary[id] = name;
+        }
+
+        Console.Write("Enter ID to find corresponding name: ");
+        uint userInputId = uint.Parse(Console.ReadLine());
+
+        if (idNameDictionary.ContainsKey(userInputId))
+            Console.WriteLine($"Name for ID {userInputId}: {idNameDictionary[userInputId]}");
+        else
+            Console.WriteLine("No name found for the given ID.");
+
+        developers.Sort((dev1, dev2) => String.Compare(dev1.Tool, dev2.Tool, StringComparison.Ordinal));
+    }
+}
